b8c4bab42e4d58a3ea3d65d760a98af285219fc6
EdgarsP <edgars30002@inbox.lv>
2023-02-14 13:32:37 +0200

    Initial comint

---
+++ src/test/java/ObjectPractice.java
@@ -0,0 +1,10 @@
+public class ObjectPractice
+
+{
+    public void workingWithOrders ()
+    {
+      Order firstOrder = new Order();
+      firstOrder.setName("Edgars");
+        System.out.println("Hello "+ firstOrder.getName());
+    }
+}
